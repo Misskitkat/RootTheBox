be9d805e2f9ec66eee255954da3fad22adfd96a9
Nick <n.zachary.young@gmail.com>
2025-04-10 00:41:35 -0400

    Debugging commit, collapse into parent

--- handlers/MissionsHandler.py
+++ handlers/MissionsHandler.py
@@ -504,7 +504,7 @@ class PurchaseHintHandler(BaseHandler):
                 self.render_page(
                     hint.box, info=["You can no longer purchase this hint."]
                 )
-            elif not hint.is_unlocked:
+            elif True:
                 self.render_page(
                     hint.box, info=["You cannot access this hint yet."]
                 )
